905c362a9a5f77719b8746ddf82bed19502c40ae
test <test@test.test>
2021-06-03 17:06:55 +0800

    GameFactory.createGame

--- contracts/GameFactory.sol
+++ contracts/GameFactory.sol
@@ -6,29 +6,30 @@ import './interfaces/IGameFactory.sol';
 import './Game.sol';
 
 contract GameFactory is IGameFactory {
-
-    modifier ensure(uint endTime) {
+    modifier ensure(uint256 endTime) {
         require(endTime >= block.timestamp, 'NoodleSwapFactory: EXPIRED');
         _;
     }
 
-    event _GameCreated(address _token,
+    event _GameCreated(
+        address _token,
         string _gameName,
         string[] _optionName,
-        uint[] _optionNum,
+        uint256[] _optionNum,
         string _resultSource,
-        uint _endTime,
-        address _game);
+        uint256 _endTime,
+        address _game
+    );
 
     function createGame(
         address _token,
         string memory _gameName,
         string[] memory _optionName,
-        uint[] memory _optionNum,
+        uint256[] memory _optionNum,
         string memory _resultSource,
-        uint _endTime
-    )  ensure(_endTime) public override returns(address game) {
-        Game game = new Game(_token,_gameName,_optionName,_optionNum,_resultSource,_endTime);
-        emit _GameCreated(_token,_gameName, _optionName,_optionNum,_resultSource,_endTime,address(game));
+        uint256 _endTime
+    ) public override ensure(_endTime) returns (address game) {
+        game = address(new Game(_token, _gameName, _optionName, _optionNum, _resultSource, _endTime));
+        emit _GameCreated(_token, _gameName, _optionName, _optionNum, _resultSource, _endTime, game);
     }
 }
